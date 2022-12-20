13c3b26c538ddb5dd2e150f88b9dfeb6b7f2b9ff
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2022-12-20 04:57:20 +0000

    Saved progress on HelloWorld 2022-12-20 04:57:20 +0000

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -1,11 +1,8 @@
-//reimport java.util.Scanner;
 
 class HelloWorld {
  
     public static void main(String [] args) {
-    /*Scanner jx = new Scanner(System.in);
-            System.out.println("Username:");
-            String username=jx.next();*/
+   
             System.out.println("Hello Wolrd");
         }
         
