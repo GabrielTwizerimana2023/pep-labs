24b5f4f0ff7f34dc93c58b898d06e109580308e6
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2022-12-25 21:59:42 +0000

    Saved progress on GreaterOrLesser 2022-12-25 21:59:42 +0000

--- GreaterOrLesser/src/main/java/GreaterOrLesser.java
+++ GreaterOrLesser/src/main/java/GreaterOrLesser.java
@@ -1,4 +1,6 @@
 
+import java.util.Scanner;
+
 public class GreaterOrLesser {
     /**
      * Let's say part of a calculator app has to judge whether one number is bigger than another. This problem will
@@ -9,7 +11,24 @@ public class GreaterOrLesser {
      * @return  If a is less than b, return the string "Lesser". if a is greater than b, return "Greater".
      *          If neither is true, return "Equal". This problem is case-sensitive!
      */
+    public static void main(String [] args){
+        GreaterOrLesser cs=new GreaterOrLesser();
+        Scanner z=new Scanner(System.in);
+        System.out.println("enter number a");
+        int a=z.nextInt();
+        Scanner y=new Scanner(System.in);
+        System.out.println("enter number b");
+        int b=z.nextInt();
+        cs.decide(a, b);
+        System.out.println(cs.decide(a, b));
+    }
     public String decide(int a, int b){
+        if(a>b){
+            return "Greater";
+        }
+        else if(b>a){
+            return "Lesser";
+        }else{
         return "Equal";
     }
-}
+}}
