c3a3ea21bb5fecc7166049cb333823d765b22aea
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2022-12-25 19:54:55 +0000

    Saved progress on Add 2022-12-25 19:54:55 +0000

--- Add/src/main/java/Add.java
+++ Add/src/main/java/Add.java
@@ -1,3 +1,5 @@
+//import org.eclipse.jetty.util.Scanner;
+import java.util.Scanner;
 
 public class Add {
     /**
@@ -10,7 +12,21 @@ public class Add {
      * @param b second number to be added.
      * @return the sum of a and b.
      */
+    public static void main(String [] args){
+        Add y=new Add();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number a");
+        int a=sc.nextInt();
+        Scanner sc1=new Scanner(System.in);
+        System.out.println("Enter number b");
+        int b=sc1.nextInt();
+    
+        y.addNumbers(a, b);
+        System.out.println("Sum of 2 intigers is "+y.addNumbers(a, b));
+
+    }
     public int addNumbers(int a, int b){
-        return 0;
+        int sum=a+b;
+        return sum;
     }
 }
