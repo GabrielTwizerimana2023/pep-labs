6e07df46439a4aad1de5e80f2f99639658eccbac
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-01-13 03:34:51 +0000

    Saved progress on ArraysAreEqual 2023-01-13 03:34:51 +0000

--- ArraysAreEqual/src/main/java/ArraysAreEqual.java
+++ ArraysAreEqual/src/main/java/ArraysAreEqual.java
@@ -15,6 +15,15 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
+       int[] arr1={1,2,3,4,5};
+       int[] arr2={1,2,3,4,5};
+       if(a.length!=b.length)
+       return false;
+       for(int i=0;i<a.length;i++){
+        if(a[i]!=b[i]){
         return false;
+        }
+       }
+        return true;
     }
 }
