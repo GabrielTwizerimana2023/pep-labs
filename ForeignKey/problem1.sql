9f8712fdfd4cddf46d11913636aad37c527a624d
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-01-24 23:16:37 +0000

    Saved progress on FlightTracker 2023-01-24 23:16:37 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,2 +1,2 @@
-create table post (id integer, post varchar(255),fk_user integer);
-create table user(id integer primary key, firstname varchar(100),lastname varchar(100));
+create table post (id integer, post varchar(255),fk_user integer references user(id));
+--create table user(id integer primary key, firstname varchar(100),lastname varchar(100));
