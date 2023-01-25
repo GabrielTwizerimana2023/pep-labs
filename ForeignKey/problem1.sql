57d74ca8f67f04474e07b0ce7616acbc08622739
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-01-25 20:30:27 +0000

    Saved progress on InnerJoin 2023-01-25 20:30:27 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,2 +1,4 @@
-create table post (id integer, post varchar(255),fk_user integer references user(id));
---create table user(id integer primary key, firstname varchar(100),lastname varchar(100));
+CREATE TABLE user (id SERIAL PRIMARY KEY, firstname varchar(100), lastname varchar(100));
+INSERT INTO user (id,firstname, lastname) VALUES (1,'Gabriel', 'Twizerimana');
+CREATE TABLE post (id SERIAL PRIMARY KEY,post VARCHAR(255),user_fk int REFERENCES user(id));
+INSERT INTO post (post, user_fk) VALUES ('test post123', 1);
