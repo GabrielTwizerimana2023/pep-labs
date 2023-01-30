d38e7a58de01e8ffb73ade28de8d1ac34dcb508f
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-01-30 21:57:39 +0000

    Saved progress on FlightTracker 2023-01-30 21:57:39 +0000

--- FlightTracker/src/main/java/Application/Service/FlightService.java
+++ FlightTracker/src/main/java/Application/Service/FlightService.java
@@ -51,30 +51,11 @@ public class FlightService {
      * @return the newly added flight if the add operation was successful, including the flight_id. We do this to
      *         inform our provide the front-end client with information about the added Flight.
      */
-    public Flight addFlight(Flight flight){
-        Connection connection = ConnectionUtil.getConnection();
-        try {
-            //Write SQL logic here. When inserting, you only need to define the departure_city and arrival_city
-            //values (two columns total!)
-            String sql = "insert into flight(departure_city,arrival_city) values (?,?);" ;
-            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
-            //write preparedStatement's setString and setInt methods here.
-            preparedStatement.setString(1, flight.getDeparture_city());
-            preparedStatement.setString(2, flight.getArrival_city());
-
-            preparedStatement.executeUpdate();
-            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
-            if(pkeyResultSet.next()){
-                int generated_flight_id = (int) pkeyResultSet.getLong(1);
-                return new Flight(generated_flight_id, flight.getDeparture_city(), flight.getArrival_city());
-            }
-        }catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        return null;
-       
+    public Flight addFlight(Flight flight){ 
+   // flightDAO.insertFlight(flight);
+        return flightDAO.insertFlight(flight);
     }
+    
 
     /**
      * TODO: Use the FlightDAO to update an existing flight from the database.
@@ -89,24 +70,11 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        Connection connection = ConnectionUtil.getConnection();
-        FlightDAO flightdao=new FlightDAO();
-        try {
-            //Write SQL logic here
-            String sql = "update flight set departure_city=?,arrival_city=? where flight_id=?;";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-            preparedStatement.setString(1, flight.getDeparture_city());
-            preparedStatement.setString(2, flight.getArrival_city());
-            preparedStatement.setInt(3, flight_id);
-            preparedStatement.executeUpdate();
-            
-        }catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        return null;
+    if(flightDAO.getFlightById(flight_id)!=null){
+     return flightDAO.getFlightById(flight_id);
     }
-
+    return null;
+    }
     /**
      * TODO: Use the FlightDAO to retrieve a List containing all flights.
      * You could use the flightDAO.getAllFlights method.
@@ -114,22 +82,8 @@ public class FlightService {
      * @return all flights in the database.
      */
     public List<Flight> getAllFlights(){
-        Connection connection = ConnectionUtil.getConnection();
-        List<Flight> flights=new ArrayList<>();
-        try {
-            String sql="select * from flight;";
-        PreparedStatement preparedstatement=connection.prepareStatement(sql);
-        ResultSet rs=preparedstatement.executeQuery();
-        while (rs.next()) {
-            Flight flight = new Flight(rs.getInt("flight_id"), rs.getString("departure_city"),
-                        rs.getString("arrival_city"));
-                flights.add(flight);
-        }
-        } catch (Exception e) {
-           System.out.println(e.getMessage());
-        }
         
-        return flights;
+        return flightDAO.getAllFlights();
     }
 
     /**
@@ -141,25 +95,6 @@ public class FlightService {
      * @return all flights departing from departure_city and arriving at arrival_city.
      */
     public List<Flight> getAllFlightsFromCityToCity(String departure_city, String arrival_city) {
-        Connection connection = ConnectionUtil.getConnection();
-        List<Flight> flights = new ArrayList<>();
-        try {
-            String sql = "select * from flight where departure_city=? and arrival_city=?;";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-            //write PreparedStatement setString and setInt methods here.
-            preparedStatement.setString(1,departure_city);
-            preparedStatement.setString(2, arrival_city);
-
-            ResultSet rs = preparedStatement.executeQuery();
-            while(rs.next()){
-                Flight flight = new Flight(rs.getInt("flight_id"), rs.getString("departure_city"),
-                        rs.getString("arrival_city"));
-                flights.add(flight);
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-        return flights;
+       return flightDAO.getAllFlightsFromCityToCity(departure_city, arrival_city);
     }
 }
