538ee7690b54f6ecae7fb188b05a7ca2c6a6fb3e
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-01-30 01:24:00 +0000

    Saved progress on CombineArrays 2023-01-30 01:24:00 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,7 +8,19 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+    
+        int a1=arr1.length;
+        int a2=arr2.length;
+        int b=a1+a2;
+        int[] b1=new int[b];
+        for(int i=0;i<a1;i++){
+        b1[i]=arr1[i];
+        }
+        for(int j=0;j<a2;j++){
+        b1[a1+j]=arr2[j];
+        }
+      
+        
+        return b1;
     }
 }
