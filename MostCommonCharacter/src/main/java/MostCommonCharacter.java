d49d12d69d981a72da1605fa494b8eec9a00fec7
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-02-05 22:43:28 +0000

    Saved progress on MostCommonCharacter 2023-02-05 22:43:28 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.HashMap;
+
+import com.azul.crs.runtime.utils.PackedDataEntriesMap.Entry;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +12,30 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>(); 
+         char maxappearchar = ' ';
+         for (int i = 0; i < str.length(); i++)
+         {
+              if ( map.containsKey(str.charAt(i)))
+              {
+                    map.put (str.charAt(i), map.get(str.charAt(i)) + 1 );
+              }       
+              else
+              {
+                map.put (str.charAt(i), 1);
+              }    
+              int maxValueInMap=(Collections.max(map.values()));  // This will return max value in the Hashmap    
+              for (java.util.Map.Entry<Character, Integer> entry : map.entrySet())
+              {  
+                  if (entry.getValue()==maxValueInMap)
+                  {
+                      System.out.println("the max char is : " + entry.getKey() + "  and displayed  " +maxValueInMap+ "  times");     // Print the key with max value
+                      maxappearchar = entry.getKey();
+                  }
+  
+              }
+              
+      }
+      return maxappearchar;
     }
 }
