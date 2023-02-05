6cc3038cb2afd718ad5c0f3b54274f42173563d9
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-02-05 21:36:46 +0000

    Saved progress on Fibonacci 2023-02-05 21:36:46 +0000

--- Fibonacci/src/main/java/Fibonacci.java
+++ Fibonacci/src/main/java/Fibonacci.java
@@ -15,6 +15,27 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        {
+            /* Declare an array to store Fibonacci numbers. */
+        int f[] = new int[n + 1];
+        int i;
+ 
+        /* 0th and 1st number of the series are 0 and 1*/
+        f[0] = 0;
+ 
+        if (n > 0) {
+            f[1] = 1;
+ 
+            for (i = 2; i <= n; i++) {
+                /* Add the previous 2 numbers in the series
+             and store it */
+                f[i] = f[i - 1] + f[i - 2];
+            }
+        }
+ 
+        return f[n];
     }
-}
+        }
+       
+    }
+
