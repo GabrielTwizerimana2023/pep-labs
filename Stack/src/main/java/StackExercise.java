1888e0c6b21fa0313dc2c0dbe6d68d332286202d
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-02-13 02:19:54 +0000

    Saved progress on Stack 2023-02-13 02:19:54 +0000

--- Stack/src/main/java/StackExercise.java
+++ Stack/src/main/java/StackExercise.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 /**
  * Stacks are LIFO - last in, first out, like a stack of plates or books, or the puzzle game "Towers of Hanoi".
  * They are used in algorithms where it is needed to keep track of the ordering in which the elements were inserted.
@@ -19,11 +20,10 @@ public class StackExercise {
      *
      * @return a newly instantiated Deque. Deques implement both stack and queue behavior.
      */
+   
     public Deque<String> createNewStack(){
-        StackExercise cs=new StackExercise();
-        Deque<String> stack=new ArrayDeque<>();
-        stack.add("Gaby");
-        return stack;
+        Deque<String> stacks=new ArrayDeque<>();
+        return stacks;
     }
 
     /**
@@ -31,10 +31,7 @@ public class StackExercise {
      * @return the number of elements in the deque.
      */
     public int getSize(Deque<String> stack){
-        StackExercise st=new StackExercise();
-        stack=st.createNewStack();
-        stack.size();
-        
+    
         return stack.size();
     }
 
@@ -44,11 +41,8 @@ public class StackExercise {
      * @param item a String to be added to the deque.
      */
     public void addToTopOfStack(Deque<String> stack, String item){
-        StackExercise st=new StackExercise();
-        stack=st.createNewStack();
-        stack.add("Yvonne");
-        stack.add("Brielle");
-        System.out.println(stack);
+     stack.addFirst(item);
+     
     }
 
     /**
@@ -58,14 +52,9 @@ public class StackExercise {
      * @return the value popped from the top of the deque.
      */
     public String removeFromTopOfStack(Deque<String> stack){
-        StackExercise st=new StackExercise();
-        stack=st.createNewStack();
-        stack.add("Yvonne");
-        stack.add("Brielle");
-        String ebook=st.removeFromTopOfStack(stack);
-        System.out.println(st.getSize(stack));
-        System.out.println(ebook);
-        return "";
+    
+        return stack.pop();
+        
     }
     /**
      * Return the value at the top of a deque without removing the value.
@@ -73,6 +62,6 @@ public class StackExercise {
      * @return the value peeked from the top of the deque.
      */
     public String getTopOfStackWithoutRemoving(Deque<String> stack){
-        return "";
+        return stack.peek();
     }
 }
