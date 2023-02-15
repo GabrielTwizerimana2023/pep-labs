5cf06850b874f1f8ecebdc4cf42f163713545436
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-02-15 03:44:37 +0000

    Saved progress on PathParameters 2023-02-15 03:44:37 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -1,10 +1,8 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
-
 public class LargestSum {
+    private static final int INT_MIN = 0;
     /**
      * Get the largest possible sum that can be obtained from a pair of values in the list. A number can't be added
      * to itself, unless there are duplicates.
@@ -12,8 +10,7 @@ public class LargestSum {
      * @param nums a list of ints.
      * @return the largest possible sum of separate numbers from nums.
      */
-    public int bigSum(ArrayList<Integer> nums){
-
-        return 0;
+    public int bigSum(List<Integer> nums){
+    return 
 }
 }
