687ccdd9ca50ec1a8126451e038b2935be5e1d64
Gabriel Twizerimna <gabyjobseekig2022@gmail.com>
2023-02-17 21:08:17 +0000

    Saved progress on PigLatin 2023-02-17 21:08:17 +0000

--- PathParameters/src/main/java/com/revature/JavalinSingleton.java
+++ PathParameters/src/main/java/com/revature/JavalinSingleton.java
@@ -14,15 +14,14 @@ public class JavalinSingleton {
          * 
          * Note: Please refer to the "PathParameters.MD" file for more assistance if needed.
          */
-        app.get("/firstname/{first}", ctx -> {
-            
+        app.get("/firstname/\\{first}", ctx -> {
+            String name=ctx.pathParam("first");
+            ctx.result(name);
             //write code here
-            String firstName=ctx.pathParam("first");
-             ctx.result(firstName);
+
         });
 
 
         return app;
     }
-    
 }
